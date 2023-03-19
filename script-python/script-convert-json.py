84f3a4b6a57c7220242e9dd14cf94376de54cf31
Guiherme de Oliveira <guilherme.oliver12@gmail.com>
2023-03-18 22:41:33 -0300

    finalizado script python para enviar dados para o s3 aws

--- script-python/script-convert-json.py
+++ script-python/script-convert-json.py
@@ -4,30 +4,41 @@ Created on Fri Mar 17 05:42:24 2023
 
 @author: guilh
 """
+#importando packages
 import os
 import json
 import requests
+import boto3
 
-"""
-request = requests.get("http://localhost/MyAPI/api/Produtos/").text
-request_formated = request[865:]
-json_object = json.loads(request_formated)
-dados = json_object['data']
-#print(dados)
+#parâmetros para acessar o bucket s3
+s3 = boto3.resource(
+    service_name='s3',
+    region_name='sa-east-1',
+    aws_access_key_id = 'AKIAVH6CMGFCEERXKVGK',
+    aws_secret_access_key='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'
+)
 
-file = open("result.json", "a")
-file.write(request_formated)
-file.close()
-"""
+#função para obtêr os dados da requisição da API php
+def datajson():
+    request = requests.get("http://localhost/MyAPI/api/Produtos/").text
+    request_formated = request[865:]
+    json_object = json.loads(request_formated)
+    data_json = json_object['data']
+    return data_json
 
+#função que converte o resultado da requisição da API em uma string texto
 def convertjson():
     request = requests.get("http://localhost/MyAPI/api/Produtos/").text
     request_formated = request[865:]
     json_object = json.loads(request_formated)
     return request_formated
 
+#função que escreve os dados em um arquivo local json
 def filewrite():
-    file = open("result.json", "a+")
+    try:
+      file = open("result.json", "a+")
+    except OSError:
+       return print("Não deu para abrir o arquivo")  
     if(os.path.getsize('result.json') > 0):
         file.truncate(0)
         file.write(convertjson())
@@ -36,5 +47,11 @@ def filewrite():
     file.close()
     return "ok"
 
+#função envia objeto json para um bucket s3 na AWS
+def sendtoaws():
+    bucket = 'bucket-market-data'
+    s3.Object(bucket, 'result.json').upload_file(Filename = 'result.json')
+
 dados = convertjson()
 result = filewrite()
+awsenvy = sendtoaws()
